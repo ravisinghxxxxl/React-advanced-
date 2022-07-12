53a0b89a63cf2227f8e9581cfa239dd6161014bb
Ravinder <ravisinghxxxxl@gmail.com>
2022-07-12 03:41:32 +0300

    Updates

---
+++ precache-manifest.ae7b0b4b170374af108eb8d58409a480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7a462aabf88675efaf7",
+    "url": "/React-advanced-/static/css/main.67a860bd.chunk.css"
+  },
+  {
+    "revision": "e7a462aabf88675efaf7",
+    "url": "/React-advanced-/static/js/main.e7a462aa.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/React-advanced-/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/React-advanced-/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "bf9c5949faf4917e3203",
+    "url": "/React-advanced-/static/js/runtime~main.bf9c5949.js"
+  },
+  {
+    "revision": "2dd1d4faab2d96736ed9ad26fbbebee0",
+    "url": "/React-advanced-/index.html"
+  }
+];
